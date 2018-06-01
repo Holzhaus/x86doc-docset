87b97e5a5993934d07946f25710322c2332804b3
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2018-06-01 08:05:17 +0200

    Use gen-docset.sh to generate whole docset directory structure

--- gen-docset.sh
+++ gen-docset.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 DOCSET=Intel_x86_IA32
+mkdir -p "${DOCSET}.docset/Contents/Resources/Documents"
+cp "icon.png" "${DOCSET}.docset"
+cp "Info.plist" "${DOCSET}.docset/Contents"
 wget --verbose --mirror --no-host-directories --cut-dirs 1 --page-requisites \
      --convert-links --no-parent \
      --directory-prefix "${DOCSET}.docset/Contents/Resources/Documents/" \
